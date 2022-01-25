de7b61ba474e56af5d8d86aeed3b2bbf6fc73d35
dino9611 <aldinorahman36@gmail.com>
2022-01-25 10:58:36 +0700

    day1

---
+++ index.js
@@ -0,0 +1,12 @@
+// var gunanya untuk initialisasi tempat didalam ram
+var namaOrang = `dino`;
+// console.log("daniel");
+namaOrang = "rio";
+// console.log(namaOrang);
+// numbers
+var usia = 25;
+console.log(usia);
+
+// boolean (bisa true/false)
+var isLecturer = true 
+console.log(isLecturer)
