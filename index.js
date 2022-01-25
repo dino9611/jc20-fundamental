bfc5ffe48ba28f0bf513e10c8517ee3139c1e39a
dino9611 <aldinorahman36@gmail.com>
2022-01-25 12:06:48 +0700

    day1

--- index.js
+++ index.js
@@ -5,8 +5,46 @@ namaOrang = "rio";
 // console.log(namaOrang);
 // numbers
 var usia = 25;
-console.log(usia);
+// console.log(usia);
 
 // boolean (bisa true/false)
 var isLecturer = true 
-console.log(isLecturer)
+// console.log(isLecturer)
+var tipe = typeof(isLecturer)
+// console.log('line 14 ',tipe)
+// console.log('line 15' ,typeof(tipe))
+
+var angka1 = 5 
+var angka2 = 7
+var hasil =angka1*angka2
+// console.log(hasil)
+
+var murid1 = 'dika'
+var murid2 = "doni"
+// console.log(murid1+' '+murid2)
+// console.log(`${murid1} ${murid2}`)
+
+// console.log('11'+1)
+// console.log('11'/2)
+
+// console.log(7%100)
+
+var angka1 = 5
+// tambah 1
+// angka1++
+// angka1 = angka1+1
+angka1+=10
+// sama dengan
+// angka1=angka1+10
+
+
+// console.log(Math.round(Math.random()*10))
+var numb = '112.02'
+var numb2=112
+numb2= numb2 +''
+// console.log(parseInt(numb))
+// console.log(Number(numb))
+// console.log(numb2)
+var x= 'Hello worlds'
+console.log(x.slice(5,8))
+console.log(x.split(' '))
